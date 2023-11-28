155a003a2c228563a509d1a0a3e537c54503bae7
mariya <mariya.bychek@yandex.by>
2023-11-28 18:49:28 +0300

    guess

--- guess.py
+++ guess.py
@@ -1,19 +1,23 @@
 from random import randint
-def ran():
-    a=int(input("start"))
-    b=int(input("stop"))
-    return b,a
 
-def randik (ran):
-    (b,a)=ran()
-    if 30 >=b-a and b-a>= 4:
+
+def ran():
+    a = int(input("start"))
+    b = int(input("stop"))
+    return b, a
+
+
+def randik(ran):
+    (b, a) = ran()
+    if 30 >= b - a and b - a >= 4:
         first = int(randint(a, b))
         second = int(randint(a, b))
         third = int(randint(a, b))
-        c=(first,second,third)
+        c = (first, second, third)
         print(c)
     else:
         quit()
+
     def your_choice():
         while True:
             a1 = input("your first number")
@@ -26,16 +30,19 @@ def randik (ran):
             if 'exit' in c1:
                 quit()
             if int(a1) == first and second == int(b1) and third == int(c1):
-                    print("You win!")
-                    break
+                print("You win!")
+                break
             else:
-                    print('Try again!')
-            list1=[a1,b1,c1]
+                print('Try again!')
+            list1 = [a1, b1, c1]
             list2 = [first, second, third]
             match = 0
             for x in list1:
                 if int(x) in list2:
-                  match = match + 1
+                    match = match + 1
             print(f'Matched numbers={match}')
+
     print(your_choice())
+
+
 randik(ran)
